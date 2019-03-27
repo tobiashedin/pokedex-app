313c4a1dadc1390a442d87287d4f1c5835905d0d
Tobias Hedin <tobias.hedin@gmail.com>
2019-03-27 16:45:32 +0100

    Updates

---
+++ precache-manifest.3ae16c6b6138344acc6cb00d8493ffef.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e758e5600d5f7d6a5fc8",
+    "url": "/pokedex-app/static/js/runtime~main.dc1f8bd6.js"
+  },
+  {
+    "revision": "4368922f8952b7989f79",
+    "url": "/pokedex-app/static/js/main.e009533c.chunk.js"
+  },
+  {
+    "revision": "3c688a83c6a96b1bd515",
+    "url": "/pokedex-app/static/js/2.f885ce20.chunk.js"
+  },
+  {
+    "revision": "4368922f8952b7989f79",
+    "url": "/pokedex-app/static/css/main.834ce8e9.chunk.css"
+  },
+  {
+    "revision": "c677e98d98c8a83373743ea35053ae85",
+    "url": "/pokedex-app/index.html"
+  }
+];
